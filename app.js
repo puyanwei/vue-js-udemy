17d5a8b7b4f0527c16c3717491569e091ef966b6
Puyan Wei <puyanwei@gmail.com>
2018-03-21 13:54:43 +0000

    interactive styling using text box to set colour and width of boxes

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ window.onload = function() {
   new Vue({
     el: "#app",
     data: {
-      makeRed: false
+      width: 100,
+      color: "gray"
+    },
+    computed: {
+      myStyle: function() {
+        return {
+          backgroundColor: this.color,
+          width: this.width + "px"
+        };
+      }
     }
   });
 };
