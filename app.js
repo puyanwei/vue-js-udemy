00978143ab5516d4b9ef58f6c118715ce988c0f5
Puyan Wei <puyanwei@gmail.com>
2018-03-21 11:10:20 +0000

    exercise 3 - using computed properties and watch

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ window.onload = function() {
   new Vue({
     el: "#app",
     data: {
-      value: ""
+      value: 0
     },
-    methods: {
-      alertMe: function() {
-        alert("Alert!");
-      },
-      addValue: function() {
-        this.value = event.target.value;
+    computed: {
+      result: function() {
+        if (this.value === 37) {
+          return (this.result = "done");
+        } else {
+          return "not there yet";
+        }
+      }
+    },
+    watch: {
+      result: function() {
+        var vm = this;
+        setTimeout(function() {
+          vm.value = 0;
+        }, 5000);
       }
     }
   });
