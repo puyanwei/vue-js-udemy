65cb88c84745620ee549c991f0f6ca2ef5a51845
Puyan Wei <puyanwei@gmail.com>
2018-03-21 13:24:55 +0000

    dynamic styling with css

--- app.js
+++ app.js
@@ -2,24 +2,7 @@ window.onload = function() {
   new Vue({
     el: "#app",
     data: {
-      value: 0
-    },
-    computed: {
-      result: function() {
-        if (this.value === 37) {
-          return (this.result = "done");
-        } else {
-          return "not there yet";
-        }
-      }
-    },
-    watch: {
-      result: function() {
-        var vm = this;
-        setTimeout(function() {
-          vm.value = 0;
-        }, 5000);
-      }
+      makeRed: false
     }
   });
 };
