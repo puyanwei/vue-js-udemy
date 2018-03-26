ec2e33cf1ee8bc2da7418fae866a023db3fb6599
Puyan Wei <puyanwei@gmail.com>
2018-03-26 10:33:45 +0100

    using v-if and v-else

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-window.onload = function() {
-  new Vue({
-    el: "#app",
+new Vue({
+    el: '#app',
     data: {
-      effects: {
-        highlight: false,
-        shrink: true
-      }
+        show: true,
     },
-    methods: {
-      toggleEffect: function() {
-        var vm = this;
-        setInterval(function() {
-          vm.effects.highlight = !vm.effects.highlight;
-          vm.effects.shrink = !vm.effects.shrink;
-        }, 1000);
-      }
-    }
-  });
-};
+});
